5807cd066725f49534a5194c5c2cf41c0c15f2ac
Muneeb Haq <muh5329@gmail.com>
2024-06-10 21:40:07 -0400

    Stocker class init

---
+++ dtp-controll-panel/dtp_controll_panel/stocker.py
@@ -0,0 +1,12 @@
+
+"""
+    Creates a snapshot of stocks and their current values , displays current snapshot as a pdf
+"""
+from typing import List
+
+
+class Stocker:
+  def __init__(self, stocks : List[str]):
+    self.stocks = stocks
+
+
