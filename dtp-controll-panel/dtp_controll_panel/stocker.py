c46bdccfec336819ece1fe72f86eadb7648def83
Muneeb Haq <muh5329@gmail.com>
2024-06-11 21:48:08 -0400

    define what the stocker class is

--- dtp-controll-panel/dtp_controll_panel/stocker.py
+++ dtp-controll-panel/dtp_controll_panel/stocker.py
@@ -6,7 +6,20 @@ from typing import List
 
 
 class Stocker:
-  def __init__(self, stocks : List[str]):
+  """  
+  
+    Initializes the stocker class.
+    
+    The Stocker class contains functionality to generate the current stock value,
+      upward/downward trend, theta's, optionality etc. of a given set of chosen stocks
+
+      It also has the ability to render line by line these values in a pdf file, for 
+        use by the dtp-controll-panel app
+
+  """
+  def __init__(self, stocks  : List[str] = []):
     self.stocks = stocks
 
 
+  def generate_daily_values(self):
+    do="something"
